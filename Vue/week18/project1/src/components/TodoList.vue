fe7ca5213ad865e16bc9ff0a9a84a4917a6203b3
yangwonseok98 <diddnjstjr11@naver.com>
2023-11-13 12:13:47 +0900

    202311131213

---
+++ Vue/week18/project1/src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<!--TodoList.vue-->
+<template>
+  <div>
+    <TodoListItem v-for="todo in store.todos" :key="todo.id" :todo="todo" />
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+import TodoListItem from "@/components/TodoListItem.vue";
+
+const store = useCounterStore();
+</script>
+
+<style lang="scss" scoped></style>
